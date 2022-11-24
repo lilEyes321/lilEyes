92f8fbd074030e3ca3623992597a363e4c23f031
lilEyes321 <118958967+lilEyes321@users.noreply.github.com>
2022-11-24 00:08:15 -0600

    gf

---
+++ asset/js/init.js
@@ -0,0 +1,16 @@
+
+ jQuery(document).ready(function($) {
+   $('.flexslider').flexslider({
+      namespace: "flex-",
+      controlsContainer: ".flex-container",
+      animation: 'slide',
+      controlNav: true,
+      directionNav: false,
+      smoothHeight: true,
+      slideshowSpeed: 7000,
+      animationSpeed: 600,
+      randomize: false
+   });
+});
+
+
